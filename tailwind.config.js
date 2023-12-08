c19293eeb78f5af29b293ecb5a8ceca3637754cd
Hamidreza Yavarzadeh <hamid.yavarzadeh2002@gmail.com>
2023-12-08 17:19:30 +0330

    feat : Add Tailwind Content Configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,11 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+    "./build/index.html",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
